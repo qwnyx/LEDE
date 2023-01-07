74ca78198b6c7ec2211f738d6a4240fb3ef73f0d
qwnyx <77866311+qwnyx@users.noreply.github.com>
2023-01-07 19:35:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/kenzok8/small-package.git package/luci-app-passwall2
-git clone https://github.com/kenzok8/small-package.git package/luci-app-adguardhome
